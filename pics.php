b6a2c6fdc59bf70e722392c891ab8dea703672c3
Me 249004 <249004@mrundle.nfshost.com>
2017-01-24 06:01:46 +0000

    rework picture display

--- pics.php
+++ pics.php
@@ -5,6 +5,15 @@
         if (strcmp($pic, ".") == 0 || strcmp($pic, "..") == 0) {
             continue;
         }
+        //$title = str_replace('_', ' ', $pic);
+        $title = preg_replace('/[_-]/', ' ', $pic);
+        $title = preg_replace('/\..*$/', '', $title);
+        $title = ucwords($title);
+        echo "<center>";
+        echo "<h2>$title</h2>";
+        echo "<div>";
         echo "<img src=\"$pics_dir/$pic\" alt=\"$pic\" style=\"width:375px;height:567px;\">";
+        echo "</div>";
+        echo "<center>";
     }
 ?>
