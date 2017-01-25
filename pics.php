7da1f87777dead59efe82dec2d0fe40bf5c7e150
Me 249004 <249004@mrundle.nfshost.com>
2017-01-25 07:28:47 +0000

    art formatting

--- pics.php
+++ pics.php
@@ -1,3 +1,5 @@
+<html>
+<body style='background-color: #f3f4e1;'>
 <?php
     $pics_dir = "./pics";
     $pics = scandir($pics_dir, SCANDIR_SORT_ASCENDING);
@@ -9,11 +11,14 @@
         $title = preg_replace('/[_-]/', ' ', $pic);
         $title = preg_replace('/\..*$/', '', $title);
         $title = ucwords($title);
+        echo "<div style='padding-bottom: 5%'>";
         echo "<center>";
-        echo "<h2>$title</h2>";
+        echo "<h2>\"$title\"</h2>";
         echo "<div>";
-        echo "<img src=\"$pics_dir/$pic\" alt=\"$pic\" style=\"width:375px;height:567px;\">";
+        echo "<img src='$pics_dir/$pic' alt='$pic' style='width:375px;height:567px;border:10px solid black;padding:10px 10px'>";
         echo "</div>";
         echo "<center>";
+        echo "</div>";
     }
 ?>
+</body>
