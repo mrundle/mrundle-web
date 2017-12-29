fd537c1d626e5e717fabae41854f81eaefec30c5
Me 249004 <249004@mrundle.nfshost.com>
2017-12-29 07:09:51 +0000

    updates

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 </h3>
 <h3>config</h3>
     <ul>
-        <li><a href="./config/.bash_profile">.bash_profile</a></li>
-        <li><a href="./config/.tmux.conf">.tmux.conf</a></li>
-        <li><a href="./config/.vimrc">.vimrc</a></li>
+        <li><a href="https://raw.githubusercontent.com/mrundle/mrundle-dev/master/home/mrundle/.bash_profile">.bash_profile</a></li>
+        <li><a href="https://raw.githubusercontent.com/mrundle/mrundle-dev/master/home/mrundle/.tmux.conf">.tmux.conf</a></li>
+        <li><a href="https://raw.githubusercontent.com/mrundle/mrundle-dev/master/home/mrundle/.vimrc">.vimrc</a></li>
     </ul>
 <h3>tools</h3>
     <ul>
@@ -26,7 +26,7 @@
         <li><a href="./ref/errno.txt">Linux system error codes</a></li>
         <li><a href="./ref/signal.txt">Linux system signal codes</a></li>
         <li><a href="./ref/powers_of_two.txt">Powers of 2</a></li>
-        <li><a href="https://www.kernel.org/doc/Documentation/CodingStyle">Linux Kernel Coding Style</a></li>
+        <li><a href="https://www.kernel.org/doc/Documentation/process/coding-style.rst">Linux Kernel Coding Style</a></li>
         <li><a href="https://people.freebsd.org/~lstewart/articles/cpumemory.pdf">What Every Programmer Should Know About Memory</a></li>
     </ul>
 <h3>misc</h3>
@@ -34,6 +34,7 @@
         <li><a href="./notes.php">NOTES</a></li>
         <li><a href="./todo.php">TODO</a></li>
         <li><a href="./pics.php">ART</a></li>
+        <li><a href="./to_categorize.txt">(to categorize)</a></li>
     </ul>
 </body>
 </html>
